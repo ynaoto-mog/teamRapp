28bd9a9a28f630c4dd3af150138775af77d6119a
ynaoto-mog <ynaoto.engi@gmail.com>
2021-01-31 20:00:10 +0900

    add env

---
+++ src/components/GMapSelectButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <gmap v-bind:position="position" />
+  </div>
+</template>
+
+<script>
+import Gmap from "./GMap.vue";
+export default {
+  conponents: {
+    gmap: Gmap
+  },
+  data: () => ({
+    position: new Object()
+  }),
+  methods: {
+    selectPosition(lat, lng) {
+      this.position = {
+        lat: lat,
+        lng: lng
+      };
+    }
+  }
+};
+</script>
+
+<style></style>
